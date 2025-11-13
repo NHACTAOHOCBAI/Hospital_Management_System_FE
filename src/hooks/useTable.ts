141765f65c72a7f8ff86b2bdc68cf34096918cf7
ngoctruonggiang <trggg2004@gmail.com>
2025-11-13 10:38:05 +0700

    chore: change background register and login page

--- src/hooks/useTable.ts
+++ src/hooks/useTable.ts
@@ -1,64 +1,77 @@
 /* eslint-disable react-hooks/incompatible-library */
 import type { UseQueryResult } from "@tanstack/react-query";
-import { type ColumnDef, getCoreRowModel, type SortingState, useReactTable, type VisibilityState } from "@tanstack/react-table"
-import React from "react"
+import {
+  type ColumnDef,
+  getCoreRowModel,
+  type SortingState,
+  useReactTable,
+  type VisibilityState,
+} from "@tanstack/react-table";
+import React from "react";
 
 interface UseTableProps<T> {
-    use: (params: QueryParams) => UseQueryResult<{
-        pagination: {
-            total: number;
-            page: number;
-            limit: number;
-        };
-        data: T[];
-    }, Error>
-    columns: ColumnDef<T>[]
+  use: (params: QueryParams) => UseQueryResult<
+    {
+      pagination: {
+        total: number;
+        page: number;
+        limit: number;
+      };
+      data: T[];
+    },
+    Error
+  >;
+  columns: ColumnDef<T>[];
 }
 const useTable = <T>({ use, columns }: UseTableProps<T>) => {
-    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
-    const [rowSelection, setRowSelection] = React.useState({})
-    const [pagination, setPagination] = React.useState({
-        pageIndex: 0,
-        pageSize: 2,
-    })
-    const [filter, setFilter] = React.useState("")
-    const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [pagination, setPagination] = React.useState({
+    pageIndex: 0,
+    pageSize: 2,
+  });
+  const [filter, setFilter] = React.useState("");
+  const [sorting, setSorting] = React.useState<SortingState>([]);
 
-    const { data, isFetching } = use({
-        page: pagination.pageIndex + 1,
-        limit: pagination.pageSize,
-        search: filter,
-        sortBy: sorting[0]?.id,            // ✅ cột đang sort
-        sortOrder: sorting[0]?.desc ? "DESC" : "ASC", // ✅ hướng sort
-    })
-    const totalPages = React.useMemo(() => Math.ceil((data?.pagination.total ?? 0) / pagination.pageSize), [pagination, data])
-    const table = useReactTable({
-        data: data?.data || [],
-        columns,
-        pageCount: totalPages,
-        getCoreRowModel: getCoreRowModel(),
+  const { data, isFetching } = use({
+    page: pagination.pageIndex + 1,
+    limit: pagination.pageSize,
+    search: filter,
+    sortBy: sorting[0]?.id, // ✅ cột đang sort
+    sortOrder: sorting[0]?.desc ? "DESC" : "ASC", // ✅ hướng sort
+  });
+  const totalPages = React.useMemo(
+    () => Math.ceil((data?.pagination.total ?? 0) / pagination.pageSize),
+    [pagination, data]
+  );
+  const table = useReactTable({
+    data: data?.data || [],
+    columns,
+    pageCount: totalPages,
+    getCoreRowModel: getCoreRowModel(),
 
-        manualPagination: true,
-        onPaginationChange: setPagination,
+    manualPagination: true,
+    onPaginationChange: setPagination,
 
-        manualSorting: true,
-        onSortingChange: setSorting,
+    manualSorting: true,
+    onSortingChange: setSorting,
 
-        onColumnVisibilityChange: setColumnVisibility,
-        onRowSelectionChange: setRowSelection,
-        state: {
-            columnVisibility,
-            rowSelection,
-            pagination,
-            sorting
-        },
-    })
-    return {
-        table,
-        filter,
-        setFilter,
-        setPagination,
-        isFetching
-    }
-}
-export default useTable
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      columnVisibility,
+      rowSelection,
+      pagination,
+      sorting,
+    },
+  });
+  return {
+    table,
+    filter,
+    setFilter,
+    setPagination,
+    isFetching,
+  };
+};
+export default useTable;
